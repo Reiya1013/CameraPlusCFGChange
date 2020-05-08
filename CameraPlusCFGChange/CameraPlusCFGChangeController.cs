72730e6631cb8ce868bf37ed3c0571396c4c0bbb
Reiya1013 <reiya1013@gmail.com>
2020-05-09 02:37:43 +0900

    first commit

---
+++ CameraPlusCFGChange/CameraPlusCFGChangeController.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using UnityEngine;
+using BS_Utils.Utilities;
+
+namespace CameraPlusCFGChange
+{
+    /// <summary>
+    /// Monobehaviours (scripts) are added to GameObjects.
+    /// For a full list of Messages a Monobehaviour can receive from the game, see https://docs.unity3d.com/ScriptReference/MonoBehaviour.html.
+    /// </summary>
+    public class CameraPlusCFGChangeController : MonoBehaviour
+    {
+        public static CameraPlusCFGChangeController instance { get; private set; }
+
+        private string ChangedCfg;
+
+        #region Monobehaviour Messages
+        /// <summary>
+        /// Only ever called once, mainly used to initialize variables.
+        /// </summary>
+        private void Awake()
+        {
+            // For this particular MonoBehaviour, we only want one instance to exist at any time, so store a reference to it in a static property
+            //   and destroy any that are created while one already exists.
+            if (instance != null)
+            {
+                Logger.log?.Warn($"Instance of {this.GetType().Name} already exists, destroying.");
+                GameObject.DestroyImmediate(this);
+                return;
+            }
+            GameObject.DontDestroyOnLoad(this); // Don't destroy this object on scene changes
+            instance = this;
+            Logger.log?.Debug($"{name}: Awake()");
+            Settings.instance.Awake();
+        }
+        /// <summary>
+        /// Only ever called once on the first frame the script is Enabled. Start is called after any other script's Awake() and before Update().
+        /// </summary>
+        private void Start()
+        {
+            BSEvents.gameSceneLoaded += this.OnGameSceneLoaded;
+            BSEvents.menuSceneLoaded += this.OnMenuSceneLoaded;
+        }
+        private void OnGameSceneLoaded()
+        {
+            Logger.log?.Debug($"{name}: OnGameSceneLoaded Start");
+            if (Settings.instance.GameCamera_value != string.Empty && Settings.instance.OutputCamera_value != string.Empty)
+            {
+                Logger.log?.Debug($"{name}: OnGameSceneLoaded Change");
+                ReturnCFG(Settings.instance.DefaultCamera_value);
+                ChangedCFG(Settings.instance.GameCamera_value);
+                Logger.log?.Debug($"{name}: OnGameSceneLoaded Compleat");
+            }
+        }
+        private void OnMenuSceneLoaded()
+        {
+            Logger.log?.Debug($"{name}: OnMenuSceneLoaded Start");
+            if (Settings.instance.DefaultCamera_value != string.Empty && Settings.instance.OutputCamera_value != string.Empty)
+            {
+                Logger.log?.Debug($"{name}: OnMenuSceneLoaded Change");
+                ReturnCFG(Settings.instance.GameCamera_value);
+                ChangedCFG(Settings.instance.DefaultCamera_value);
+                Logger.log?.Debug($"{name}: OnMenuSceneLoaded Compleat");
+            }
+        }
+
+        private void ReturnCFG(string retrunName)
+        {
+            if (ChangedCfg == null) return;
+            try
+            {
+                Logger.log?.Debug($"{ System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\"}");
+                string directryPass = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\";
+
+                System.IO.File.Copy(directryPass + @"\UserData\CameraPlus\" + Settings.instance.OutputCamera_value, directryPass + @"\UserData\CameraPlus\" + retrunName, true);
+            }
+            catch (Exception e)
+            {
+                Logger.log?.Debug($"{e.Message}");
+            }
+        }
+
+        private void ChangedCFG(string changedName)
+        {
+            ChangedCfg = changedName;
+            try
+            {
+                Logger.log?.Debug($"{ System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\"}");
+                string directryPass = System.IO.Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().Location) + @"..\..\";
+
+                System.IO.File.Copy(directryPass + @"\UserData\CameraPlus\" + changedName, directryPass + @"\UserData\CameraPlus\" + Settings.instance.OutputCamera_value, true);
+            }
+            catch (Exception e)
+            {
+                Logger.log?.Debug($"{e.Message}");
+            }
+        }
+
+        /// <summary>
+        /// Called every frame if the script is enabled.
+        /// </summary>
+        private void Update()
+        {
+
+        }
+
+        /// <summary>
+        /// Called every frame after every other enabled script's Update().
+        /// </summary>
+        private void LateUpdate()
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the script becomes enabled and active
+        /// </summary>
+        private void OnEnable()
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the script becomes disabled or when it is being destroyed.
+        /// </summary>
+        private void OnDisable()
+        {
+
+        }
+
+        /// <summary>
+        /// Called when the script is being destroyed.
+        /// </summary>
+        private void OnDestroy()
+        {
+            Logger.log?.Debug($"{name}: OnDestroy()");
+            instance = null; // This MonoBehaviour is being destroyed, so set the static instance property to null.
+
+        }
+        #endregion
+    }
+}
---
+++ CameraPlusCFGChange/Settings.cs
@@ -0,0 +1,77 @@
+﻿using BeatSaberMarkupLanguage.Attributes;
+using BeatSaberMarkupLanguage.Parser;
+using BS_Utils.Utilities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime;
+
+namespace CameraPlusCFGChange
+{
+    class Settings : PersistentSingleton<Settings>
+    {
+        private Config config;
+
+        private string _defaultCamera;
+        [UIValue("DefaultCamera_value")]
+        public string DefaultCamera_value
+        {
+            get => _defaultCamera;
+            set
+            {
+                _defaultCamera = value;
+                config.SetString("Modes", "DefaultCamera", _defaultCamera.ToString());
+            }
+        }
+
+        private string _gameCamera;
+        [UIValue("GameCamera_value")]
+        public string GameCamera_value
+        {
+            get => _gameCamera;
+            set
+            {
+                _gameCamera = value;
+                config.SetString("Modes", "GameCamera", _gameCamera.ToString());
+            }
+        }
+
+        private string _outputCamera;
+        [UIValue("OutputCamera_value")]
+        public string OutputCamera_value
+        {
+            get => _outputCamera;
+            set
+            {
+                _outputCamera = value;
+                config.SetString("Modes", "OutputCamera", _outputCamera.ToString());
+            }
+        }
+        public void Awake()
+        {
+            config = new Config("CameraPlusCFGChange");
+            config.GetString("Modes", "MaterialChange", "ON");
+            string getStr = config.GetString("Modes", "DefaultCamera", "");
+            if (getStr != string.Empty)
+                DefaultCamera_value = getStr;
+            else
+                DefaultCamera_value = "cameraplus.cfg";
+
+
+            getStr = config.GetString("Modes", "GameCamera", "");
+            if (getStr != string.Empty)
+                GameCamera_value = getStr;
+            else
+                GameCamera_value = string.Empty;
+
+
+            getStr = config.GetString("Modes", "OutputCamera", "");
+            if (getStr != string.Empty)
+                OutputCamera_value = getStr;
+            else
+                OutputCamera_value = string.Empty;
+        }
+
+
+    }
+}
